5a58cb8f263c9e8f46c9ce1b034e8c94067f5785
Christopher Yoon <thechrisyoon08@gmail.com>
2019-04-05 20:48:42 -0400

    test on simple_tag

---
+++ MADDPG/main.py
@@ -0,0 +1,19 @@
+from multiagent.environment import MultiAgentEnv
+import multiagent.scenarios as scenarios
+from maddpg import MADDPG
+
+def make_env(scenario_name, benchmark=False):
+    # load scenario from script
+    scenario = scenarios.load(scenario_name + ".py").Scenario()
+    # create world
+    world = scenario.make_world()
+    # create multiagent environment
+    if benchmark:
+        env = MultiAgentEnv(world, scenario.reset_world, scenario.reward, scenario.observation, scenario.benchmark_data)
+    else:
+        env = MultiAgentEnv(world, scenario.reset_world, scenario.reward, scenario.observation)
+    return env
+
+env = make_env(scenario_name="simple_tag")
+agents = MADDPG(env, env.n)
+agents.train(max_episodes=100, max_steps=100, batch_size=50)
