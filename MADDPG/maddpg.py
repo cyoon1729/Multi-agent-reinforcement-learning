bbcc59737abfd44177344755a4c7fd52f47456cc
Christopher Yoon <thechrisyoon08@gmail.com>
2019-04-05 20:48:12 -0400

    finish updates

--- MADDPG/maddpg.py
+++ MADDPG/maddpg.py
@@ -5,7 +5,7 @@ from utils import *
 import torch 
 
 """
-TODO: Fix the policy update mess. nested arrays
+TODO: try on environment
 """
 
 class MADDPG():
@@ -22,15 +22,16 @@ class MADDPG():
         actions = []
         for agent, state in zip(self.agents, states):
             action = agent.get_actor_output(state)
+            action = np.clip(action, -1, 1)
+            action_one_hot = np.zeros(action.shape)
+            action_one_hot[action.argmax()] = 1
+            actions.append(action_one_hot)
             actions.append(action)
         actions = np.array(actions)
 
         return actions
-    
-    def get_one_hot_actions(self):
-        pass
 
-    def update(self, batch_size):
+    def update(self, batch_size, timestep):
         experiences = self.replay_buffer.sample(batch_size)
         states_batch, actions_batch, rewards_batch, next_states_batch, done_batch = experiences
 
@@ -38,7 +39,6 @@ class MADDPG():
         action_batch = [np.concatenate(actions) for actions in actions_batch] # Concatenate action vectors of all agents
         next_state_batch = [np.concatenate(next_states) for next_states in next_states_batch]
 
-        # To tensors
         total_state_batch = torch.FloatTensor(total_state_batch)
         action_batch = torch.FloatTensor(action_batch)
         next_state_batch = torch.FloatTensor(next_state_batch)
@@ -46,8 +46,10 @@ class MADDPG():
         for agent in self.agents:
             
             """
-            Obtain actions of all agents using current policies. 
-            Psuedocode:
+            Obtain actions of all agents using current policies.
+            
+            state_batch = [ [[n1] [n2] [n3] [n4] ... [nn]], [[n1] [n2] [n3] [n4] .. [nn]], .... ], states in state_batch = [[n1] [n2] [n3] [n4] ... [nn]] 
+            
             1. for each state in state_batch:
                   Obtain action output (tensor) of each agent for all agents and store in tuple -> ( Tensor([a1]), Tensor([a2]), ..., Tensor([an]) ), where ai is action of agent i
                   concatenate tuple so that we have all actions in a single tensor -> Tensor([a1, a2, ... , an])
@@ -60,7 +62,7 @@ class MADDPG():
             Output[2]: new_action_batch = Tensor([[a1, a2, a3, a4, ..., an]_{state_1}, [a1, a2, a3, a4, ..., an]_{state_2}, ..., [a1, a2, a3, a4, ..., an]_{state_n}])      
             """
             current_action_batch = []
-            for states in states_batch: # state_batch = [ [[n1] [n2] [n3] [n4] ... [nn]], [[n1] [n2] [n3] [n4] .. [nn]], .... ], states in state_batch = [[n1] [n2] [n3] [n4] ... [nn]]
+            for states in states_batch: 
                 actions_tuple = ()
                 for _agent in self.agents:
                     indiv_state = states[_agent.agent_id]
@@ -115,37 +117,52 @@ class MADDPG():
             value_loss.backward()
             agent.critic_optimizer.step()
 
-    def train(self, config):
-        max_episodes = config.max_episodes
-        max_steps = config.max_episodes
-        batch_size = config.replay_batch_size
-        rewards = []
+            """
+            Soft parameter update
+            """ 
+            # Delayed soft parameter update
+            if timestep % 50 == 0:     
+                for target_param, param in zip(agent.actor_target.parameters(), agent.actor.parameters()):
+                    target_param.data.copy_(param.data)
 
-        for episodes in range(max_episodes):
+                for target_param, param in zip(agent.critic_target.parameters(),agent.critic.parameters()):
+                    target_param.data.copy_(param.data * agent.tau + target_param.data * (1.0 - agent.tau))
+            
+            # Soft parameter update 
+            # for target_param, param in zip(agent.actor_target.parameters(), agent.actor.parameters()):
+            #     target_param.data.copy_(param.data * agent.tau + target_param.data * (1.0 - agent.tau))
+    
+            # for target_param, param in zip(agent.critic_target.parameters(),agent.critic.parameters()):
+            #     target_param.data.copy_(param.data * agent.tau + target_param.data * (1.0 - agent.tau))
+
+    def train(self, max_episodes, max_steps, batch_size):   
+        rewards = []
+        timestep = 0
+
+        for episode in range(max_episodes):
             states = self.env.reset()
-            #self.noise.reset()
-            epiode_reward = 0
+            episode_reward = 0
             
             for steps in range(max_steps):
                 actions = self.get_actions(states)
-                #actions = [self.noise.get_action(action) for action in actions]
                 new_states, rewards, dones, _ = self.env.step(actions)
-                # we are only done when every agent is done
+                
                 if all(dones) or steps == max_steps - 1:
-                    dones = [1 for _ in range(env.n)]    
+                    dones = [1 for _ in range(self.env.n)]    
                 else:
-                    dones = [0 for _ in range(env.n)]
+                    dones = [0 for _ in range(self.env.n)]
 
-                self.replay_buffer.push(states, actions, rewards, new_states, done)
+                self.replay_buffer.push(states, actions, rewards, new_states, dones)
 
                 if len(self.replay_buffer) > batch_size:
-                    self.update(batch_size)
+                    self.update(batch_size, timestep)
                 
                 states = new_states
-                episode_reward += np.sum(rewards)
+                episode_reward += sum(rewards)
+                timestep += 1
 
-
-                if all(dones):
+                if all(dones) or steps == max_steps - 1:
                     rewards.append(episode_reward)
-                    sys.stdout.write("episode: {}, reward: {}, rolling_10_average _reward: {} \n".format(episode, np.round(episode_reward, decimals=2), np.mean(rewards[-10:])))
+                    sys.stdout.write("episode: {}  |  reward: {}  \n".format(episode, np.round(episode_reward, decimals=4)))
                     break
+                
